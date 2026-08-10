6e8c2a5d4c9f83e0263b591f2f8f4937b2793cf9
El <oloyedeelijah23@gmail.com>
2026-08-10 23:29:46 +0100

    Initial commit

---
+++ 01_Boolean_Logic/Buffer.v
@@ -0,0 +1,13 @@
+/**
+ * Buffer:
+ * out = in
+ */
+
+`default_nettype none
+module Buffer(
+	input in,
+	output out
+);
+
+	// Put your code here:
+endmodule
---
+++ 01_Boolean_Logic/Not.v
@@ -0,0 +1,13 @@
+/**
+ * Not gate:
+ * out = not in
+ */
+
+`default_nettype none
+module Not(
+	input in,
+	output out
+);
+
+	// Put your code here:
+endmodule
