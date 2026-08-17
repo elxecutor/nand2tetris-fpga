2bf263515f97efb551a5213921640ab0dbcea7c3
El <oloyedeelijah23@gmail.com>
2026-08-17 18:38:44 +0100

    Implement DMux with consistent wire naming

--- 01_Boolean_Logic/DMux.v
+++ 01_Boolean_Logic/DMux.v
@@ -13,5 +13,10 @@ module DMux(
 );
 
 	// Put your code here:
-
+	wire n_sel, in_n_sel, in_sel;
+	nand(n_sel, sel, sel);
+	nand(in_n_sel, in, n_sel);
+	nand(a, in_n_sel, in_n_sel);
+	nand(in_sel, in, sel);
+	nand(b, in_sel, in_sel);
 endmodule
