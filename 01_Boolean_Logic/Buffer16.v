b80825a66226d364427d274815b1f46a336a3f6e
El <oloyedeelijah23@gmail.com>
2026-08-19 00:58:56 +0100

    Implement Buffer16 with generate loop

--- 01_Boolean_Logic/Buffer16.v
+++ 01_Boolean_Logic/Buffer16.v
@@ -10,5 +10,13 @@ module Buffer16(
 );
 
 	// Put your code here:
+	wire [15:0] n_in;
+	genvar i;
+	generate
+		for (i=0;i<=15;i++) begin
+			nand(n_in[i], in[i], in[i]);
+			nand(out[i], n_in[i], n_in[i]);
+		end
+	endgenerate
 
 endmodule
